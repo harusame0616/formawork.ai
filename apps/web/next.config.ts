b7ab33968071a9100f571eb1da8bc27211edcf7f
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-11-04 01:53:57 +0900

    nextjs の cache components と typed route を有効化

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	cacheComponents: true,
+	typedRoutes: true,
+	cacheLife: {
+		permanent: {
+			expire: Number.MAX_VALUE,
+			revalidate: Number.MAX_VALUE,
+			stale: Number.MAX_VALUE,
+		},
+	},
 };
 
 export default nextConfig;
