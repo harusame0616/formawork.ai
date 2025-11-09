0a82751adfe188fe7edff8880648366bd3d0798d
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-11-09 23:06:01 +0900

    validate

--- apps/web/config/logger.ts
+++ apps/web/config/logger.ts
@@ -1,15 +1,16 @@
 import * as v from "valibot";
 
-
 export function getLoggerConfig() {
-	return v.parse(v.object({
-		deploymentId: v.optional(v.string()),
-		gitCommitSha: v.optional(v.string()),
-	}), {
-		// biome-ignore lint/complexity/useLiteralKeys: ts(4111)
-		deploymentId: process.env["VERCEL_DEPLOYMENT_ID"],
-		// biome-ignore lint/complexity/useLiteralKeys: ts(4111)
-		gitCommitSha: process.env["VERCEL_GIT_COMMIT_SHA"],
-	});
-
+	return v.parse(
+		v.object({
+			deploymentId: v.optional(v.string()),
+			gitCommitSha: v.optional(v.string()),
+		}),
+		{
+			// biome-ignore lint/complexity/useLiteralKeys: ts(4111)
+			deploymentId: process.env["VERCEL_DEPLOYMENT_ID"],
+			// biome-ignore lint/complexity/useLiteralKeys: ts(4111)
+			gitCommitSha: process.env["VERCEL_GIT_COMMIT_SHA"],
+		},
+	);
 }
